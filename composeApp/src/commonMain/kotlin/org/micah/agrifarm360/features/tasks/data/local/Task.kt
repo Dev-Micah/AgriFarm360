733e0791d85eeda894c49666701b9c0fc585acfb
Micah Nyabuto <atembamicah@gmail.com>
2025-11-07 23:23:45 +0300

    TaskEntity dataclass

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/data/local/Task.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/data/local/Task.kt
@@ -1,8 +1,11 @@
 package org.micah.agrifarm360.features.tasks.data.local
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 
-data class Task(
-    val id: Int,
+@Entity
+data class TaskEntity(
+    @PrimaryKey(autoGenerate = true) val id: Int =1,
     val name: String,
     val scheduleTime: Long,
     val completionState: String
