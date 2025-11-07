f903e2cd6ae9944dcaa5fbb014bf593f243acd70
Micah Nyabuto <atembamicah@gmail.com>
2025-11-07 23:18:41 +0300

    Task database

---
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/data/local/TaskDatabase.kt
@@ -0,0 +1,17 @@
+package org.micah.agrifarm360.features.tasks.data.local
+
+import androidx.room.ConstructedBy
+import androidx.room.Database
+import androidx.room.RoomDatabase
+import androidx.room.RoomDatabaseConstructor
+
+@Database(entities = [TaskEntity::class], version = 1)
+@ConstructedBy(AppDatabaseConstructor::class)
+abstract class AppDatabase : RoomDatabase() {
+    abstract fun getDao(): TaskDao
+}
+
+@Suppress("KotlinNoActualForExpect")
+expect object AppDatabaseConstructor : RoomDatabaseConstructor<AppDatabase> {
+    override fun initialize(): AppDatabase
+}
