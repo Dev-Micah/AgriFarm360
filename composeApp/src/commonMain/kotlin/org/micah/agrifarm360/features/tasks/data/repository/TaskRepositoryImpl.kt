2c39648f0e1b610d9337c458c8be5a6492bc1a6e
Micah Nyabuto <atembamicah@gmail.com>
2025-11-11 23:20:32 +0300

    Task RepositoryImpl

---
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/data/repository/TaskRepositoryImpl.kt
@@ -0,0 +1,22 @@
+package org.micah.agrifarm360.features.tasks.data.repository
+
+import kotlinx.coroutines.flow.Flow
+import org.micah.agrifarm360.features.tasks.data.local.TaskDao
+import org.micah.agrifarm360.features.tasks.data.local.TaskEntity
+import org.micah.agrifarm360.features.tasks.domain.repository.TaskRepository
+
+class TaskRepositoryImpl(private val taskDao: TaskDao) : TaskRepository{
+
+
+    override fun getAllHabits(): Flow<List<TaskEntity>> {
+        return taskDao.getAllHabits()
+    }
+
+    override suspend fun insertTask(task: TaskEntity) {
+        return taskDao.insertTask(task= task)
+    }
+
+    override suspend fun deleteTask(task: TaskEntity) {
+        return taskDao.deleteHabit(task = task)
+    }
+}
