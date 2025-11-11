ec4630445f09398808f22426ba99a83440c85628
Micah Nyabuto <atembamicah@gmail.com>
2025-11-11 23:23:56 +0300

    Dashboard TopAppBar

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/ui/screens/dashboard/presentation/DashboardScreen.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/ui/screens/dashboard/presentation/DashboardScreen.kt
@@ -1,19 +1,84 @@
 package org.micah.agrifarm360.ui.screens.dashboard.presentation
 
-import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxSize
+import agrifarm360.composeapp.generated.resources.Res
+import agrifarm360.composeapp.generated.resources.add
+import agrifarm360.composeapp.generated.resources.notifications
+import agrifarm360.composeapp.generated.resources.profileicon
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import org.micah.agrifarm360.core.navigation.Destinations
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import org.jetbrains.compose.resources.painterResource
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DashboardScreen(){
-    Box(
-        contentAlignment = Alignment.Center,
-        modifier = Modifier.fillMaxSize()
-    ){
-        Text("Dashboard")
+    Scaffold(
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = {
+                    Text("AgriFarm360",
+                        modifier = Modifier.padding(start = 8.dp),
+                        style = MaterialTheme.typography.titleLarge.copy(
+                            color = MaterialTheme.colorScheme.onSurface,
+                            fontSize = 18.sp,
+                            fontWeight = FontWeight.Bold
+                        )
+                        )
+                },
+
+                navigationIcon = {
+                    IconButton(
+                        onClick = {}
+                    ){
+                        Image(
+                            painter = painterResource(Res.drawable.profileicon),
+                            contentDescription = null,
+                            modifier = Modifier.size(28.dp)
+                        )
+                    }
+                },
+
+                actions = {
+                    IconButton(onClick = {}){
+                        Icon(
+                            painter = painterResource(Res.drawable.notifications),
+                            contentDescription = "notifications"
+                        )
+                    }
+                }
+            )
+        },
+
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {},
+                shape = RoundedCornerShape(50.dp),
+                modifier = Modifier.padding(bottom = 80.dp),
+                containerColor = MaterialTheme.colorScheme.primary
+            ){
+                Icon(
+                    painter = painterResource(Res.drawable.add),
+                    contentDescription = "Add Task",
+                    tint = Color.White
+                )
+            }
+        }
+    ) {
+
     }
 }
