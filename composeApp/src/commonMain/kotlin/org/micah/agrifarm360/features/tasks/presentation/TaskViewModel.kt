207f842d29bfbc182941ea23d5aa57f2b4d75b06
Micah Nyabuto <atembamicah@gmail.com>
2025-11-16 12:35:01 +0300

    Loadtasks function

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/presentation/TaskViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/presentation/TaskViewModel.kt
@@ -1,8 +1,12 @@
 package org.micah.agrifarm360.features.tasks.presentation
 
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import kotlinx.coroutines.flow.MutableStateFlow
 import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.launchIn
+import kotlinx.coroutines.flow.onEach
+import kotlinx.coroutines.launch
 import org.micah.agrifarm360.features.tasks.data.local.TaskEntity
 import org.micah.agrifarm360.features.tasks.domain.repository.TaskRepository
 
@@ -11,11 +15,38 @@ class TaskViewModel (private val taskRepository: TaskRepository): ViewModel(){
     private val _uiState = MutableStateFlow(TaskUiState())
     val uiState : StateFlow<TaskUiState> = _uiState
 
+    init {
+        loadTasks()
+    }
 
-    data class TaskUiState(
-        val isLoading: Boolean = false,
-        val habits: List<TaskEntity> = emptyList(),
-        val error: String? = null
-    )
+    fun addTask(task: TaskEntity){
+        viewModelScope.launch {
+            taskRepository.insertTask(task)
+        }
+    }
+
+    fun loadTasks(){
+        viewModelScope.launch {
+            _uiState.value = _uiState.value.copy(isLoading = true)
+            taskRepository.getAllTasks()
+                .onEach { tasks ->
+                    _uiState.value = _uiState.value.copy(
+                        habits = tasks,
+                        isLoading = false
+                    )
+                }
+                .launchIn(viewModelScope)
+        }
+    }
+
+    fun markTaskComplete(){}
+
 
 }
+
+
+data class TaskUiState(
+    val isLoading: Boolean = false,
+    val habits: List<TaskEntity> = emptyList(),
+    val error: String? = null
+)
