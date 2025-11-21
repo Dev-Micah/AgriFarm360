04a550678acc25492cca07b7dad16a2efec15fff
Micah Nyabuto <atembamicah@gmail.com>
2025-11-22 00:03:16 +0300

    TaskViewModel declaration in KoinModule

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/di/AppModule.kt
@@ -1,17 +1,20 @@
 package org.micah.agrifarm360.di
 
+import androidx.lifecycle.viewmodel.compose.viewModel
 import org.koin.core.context.startKoin
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 import org.micah.agrifarm360.features.tasks.data.local.TaskDatabase
 import org.micah.agrifarm360.features.tasks.data.repository.TaskRepositoryImpl
 import org.micah.agrifarm360.features.tasks.domain.repository.TaskRepository
+import org.micah.agrifarm360.features.tasks.presentation.TaskViewModel
 
 val taskDataSourceModule = module{
     single { get<TaskDatabase>().taskDao() }
     single <TaskRepository> { TaskRepositoryImpl(get()) }
 
-    //viewModel { TaskViewModel(get()) }
+    viewModelOf(::TaskViewModel)
 }
 
 
