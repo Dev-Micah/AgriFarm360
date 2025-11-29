19e202e1556441b5e9110a9d0d102ec6fbf2aca9
Micah Nyabuto <atembamicah@gmail.com>
2025-11-29 22:10:20 +0300

    Mark task complete function

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/presentation/TaskViewModel.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/features/tasks/presentation/TaskViewModel.kt
@@ -11,41 +11,37 @@ import org.micah.agrifarm360.features.tasks.data.local.TaskEntity
 import org.micah.agrifarm360.features.tasks.domain.repository.TaskRepository
 
 class TaskViewModel (private val taskRepository: TaskRepository): ViewModel(){
-    private val _tasks = MutableStateFlow<List<TaskEntity>>(emptyList())
 
     private val _uiState = MutableStateFlow(TaskUiState())
     val uiState : StateFlow<TaskUiState> = _uiState
-    val tasks: StateFlow<List<TaskEntity>> = _tasks
 
     init {
         loadTasks()
     }
 
-    fun addTask(task: TaskEntity){
+    fun addTaskEvent(task: TaskEntity){
         viewModelScope.launch {
-            taskRepository.insertTask(
-                task = task
-            )
+            taskRepository.insertTask(task)
         }
     }
 
     fun loadTasks(){
-        viewModelScope.launch {
-            _uiState.value = _uiState.value.copy(isLoading = true)
-            taskRepository.getAllTasks()
-                .onEach { tasks ->
-                    _uiState.value = _uiState.value.copy(
-                        tasks = tasks,
-                        isLoading = false
-                    )
-                }
-                .launchIn(viewModelScope)
-        }
+        _uiState.value = _uiState.value.copy(isLoading = true)
+        taskRepository.getAllTasks()
+            .onEach { tasks ->
+                _uiState.value = _uiState.value.copy(
+                    tasks = tasks,
+                    isLoading = false
+                )
+            }
+            .launchIn(viewModelScope)
     }
 
-    fun markTaskComplete(){}
-
-
+    fun markTaskComplete(task: TaskEntity){
+        viewModelScope.launch {
+            taskRepository.deleteTask(task)
+        }
+    }
 }
 
 
