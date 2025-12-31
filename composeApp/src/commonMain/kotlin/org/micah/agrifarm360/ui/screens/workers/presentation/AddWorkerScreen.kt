979f64c656384dfbf15b670c46d6f115f5bf4567
Dev-Micah <atembamicah@gmail.com>
2025-12-31 23:10:01 +0300

    Register worker screen fix

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/ui/screens/workers/presentation/AddWorkerScreen.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/ui/screens/workers/presentation/AddWorkerScreen.kt
@@ -3,8 +3,7 @@ package org.micah.agrifarm360.ui.screens.workers.presentation
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -36,15 +35,15 @@ fun AddWorkerScreen(
 
     Scaffold (
         topBar = {
-            TopAppBar(
-                title = {Text("Add WorkerScreen")}
+            CenterAlignedTopAppBar(
+                title = {Text("Register Worker")}
             )
         }
     ){ innerpadding->
         Column (
             modifier = Modifier
                 .fillMaxSize()
-                .padding(16.dp)
+                .padding(innerpadding).padding(24.dp)
                 //.verticalScroll(rememberScrollState())
         ){
             CustomOutlinedTextField(
