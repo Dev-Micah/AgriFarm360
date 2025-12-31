c5f27748ba5e5b7cbfdb377513755954317adc7e
Dev-Micah <atembamicah@gmail.com>
2025-12-31 23:08:09 +0300

    WorkersViewmodel definition in the KoinModule

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/di/AppModule.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/di/AppModule.kt
@@ -6,14 +6,22 @@ import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 import org.micah.agrifarm360.data.local.Agrifarm360Database
 import org.micah.agrifarm360.data.repository.TaskRepositoryImpl
+import org.micah.agrifarm360.data.repository.WorkersRepositoryImpl
 import org.micah.agrifarm360.domain.repository.TaskRepository
+import org.micah.agrifarm360.domain.repository.WorkerRepository
 import org.micah.agrifarm360.features.tasks.presentation.TaskViewModel
+import org.micah.agrifarm360.ui.screens.workers.presentation.WorkerViewModel
 
 val taskDataSourceModule = module{
-    single { get<Agrifarm360Database>().taskDao() }
+    single { get<Agrifarm360Database>().taskDao()}
+    single { get<Agrifarm360Database>().workerDao()}
+
     single<TaskRepository>{ TaskRepositoryImpl(taskDao = get ()) }
+    single<WorkerRepository>{ WorkersRepositoryImpl(workerDao = get ()) }
+
     //viewModelOf(::TaskViewModel)
     viewModel { TaskViewModel(taskRepository = get()) }
+    viewModel { WorkerViewModel(workerRepository = get()) }
 }
 
 
