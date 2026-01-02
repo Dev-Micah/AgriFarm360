6698fb327b38e5a9b3bfbecf9923cd81b509f7b7
Dev-Micah <atembamicah@gmail.com>
2026-01-02 22:37:33 +0300

    Changed createdAt to Long

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/data/local/entities/TaskEntity.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/data/local/entities/TaskEntity.kt
@@ -5,7 +5,7 @@ import androidx.room.PrimaryKey
 
 @Entity(tableName = "tasks")
 data class TaskEntity(
-    @PrimaryKey(autoGenerate = true) val id: Int =1,
+    @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val name: String,
-    val createdAt: String,
+    val createdAt: Long,
 )
