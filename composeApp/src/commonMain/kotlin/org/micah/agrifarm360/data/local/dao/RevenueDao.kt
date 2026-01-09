3523fa5c88ec30fec25bf5f6ac06f607ef86b53e
Dev-Micah <atembamicah@gmail.com>
2026-01-10 00:20:35 +0300

    Interface RevenueDao

--- composeApp/src/commonMain/kotlin/org/micah/agrifarm360/data/local/dao/RevenueDao.kt
+++ composeApp/src/commonMain/kotlin/org/micah/agrifarm360/data/local/dao/RevenueDao.kt
@@ -1,13 +1,29 @@
 package org.micah.agrifarm360.data.local.dao
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import androidx.room.Update
+import kotlinx.coroutines.flow.Flow
 import org.micah.agrifarm360.data.local.entities.RevenueEntity
 
 @Dao
 interface RevenueDao {
-    @Query("SELECT * FROM revenue")
-    suspend fun getAllRevenue(): List<RevenueEntity>
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertRevenue(revenue: RevenueEntity)
 
+    @Update
+    suspend fun updateRevenue(revenue: RevenueEntity)
+
+    @Delete
+    suspend fun deleteRevenue(revenue: RevenueEntity)
+
+    @Query("SELECT * FROM revenue")
+    suspend fun getAllRevenue(): Flow<List<RevenueEntity>>
+
+    @Query("SELECT SUM(amount) FROM revenue")
+    fun getTotalRevenue(): Flow<Double?>
 
 }
